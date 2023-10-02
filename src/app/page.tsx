217779a81336b1c410cf02dcf00d704e7f3f9eb7
LeeKyuSung <ks5050577@gmail.com>
2023-10-02 18:20:59 +0900

    #4.6 Modifiers for Lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,29 @@ export default function Home() {
     <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
+
+        <ul>
+          {[1, 2, 3, 4, 5].map((i) => (
+            <div
+              key={i}
+              className="flex justify-between my-2 odd:bg-gray-100 even:bg-gray-200 p-2 rounded-xl"
+            >
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
+        {["a", "b", ""].map((c, i) => (
+          <li className="bg-red-500 py-2 empty:hidden" key={i}>
+            {c}
+          </li>
+        ))}
+
         <div className="flex justify-between my-2">
           <span className="text-gray-500">Tooly Table</span>
           <span className="font-semibold">$80</span>
         </div>
+
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$99</span>

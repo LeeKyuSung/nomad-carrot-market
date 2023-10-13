449e75bff26c71427b1bf8d77052e7edd567eced
LeeKyuSung <ks5050577@gmail.com>
2023-10-13 23:11:37 +0900

    #7.2 The Register Function

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -1,53 +1,20 @@
 "use client";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Forms() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-  };
-  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setEmail(value);
-  };
-  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setPassword(value);
-  };
-
-  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(username, email, password);
-  };
+  const { register, watch } = useForm();
 
   return (
-    <form onSubmit={onSubmit}>
+    <form>
       <input
-        value={username}
-        onChange={onUsernameChange}
+        {...register("username")}
         type="text"
         placeholder="Username"
         required
       />
+      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        value={email}
-        onChange={onEmailChange}
-        type="email"
-        placeholder="Email"
-        required
-      />
-      <input
-        value={password}
-        onChange={onPasswordChange}
+        {...register("password")}
         type="password"
         placeholder="Password"
         required

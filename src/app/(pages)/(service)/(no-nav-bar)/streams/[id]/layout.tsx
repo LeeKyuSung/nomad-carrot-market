3b8078ad3a88601be7be2950bf451ff11a682ef3
LeeKyuSung <ks5050577@gmail.com>
2023-10-18 21:13:54 +0900

    #13은 스킵

--- src/app/(pages)/(service)/(no-nav-bar)/streams/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import LayoutComponent from "@/components/app-bar";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <LayoutComponent title="Lets try potatoes" canGoBack>
-      {children}
-    </LayoutComponent>
-  );
-}

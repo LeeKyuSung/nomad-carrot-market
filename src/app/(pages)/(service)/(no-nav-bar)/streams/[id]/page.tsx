0d1e3d95dc8aaf6315196b4d098ceaa12dbce6e8
LeeKyuSung <ks5050577@gmail.com>
2023-10-18 23:22:17 +0900

    Update page.tsx

--- src/app/(pages)/(service)/(no-nav-bar)/streams/[id]/page.tsx
+++ src/app/(pages)/(service)/(no-nav-bar)/streams/[id]/page.tsx
@@ -28,7 +28,9 @@ interface MessageForm {
 }
 export default function StreamDetail({ params }: { params: { id: string } }) {
   const { user } = useUser();
-  const { data, mutate } = useSWR<StreamResponse>(`/api/streams/${params.id}`);
+  const { data, mutate } = useSWR<StreamResponse>(`/api/streams/${params.id}`, {
+    refreshInterval: 5000,
+  });
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${params.id}/message`
@@ -36,56 +38,72 @@ export default function StreamDetail({ params }: { params: { id: string } }) {
   const onValid = (form: MessageForm) => {
     if (loading) return;
     reset();
+    mutate(
+      (prev) =>
+        prev &&
+        ({
+          ...prev,
+          stream: {
+            ...prev.stream,
+            Message: [
+              ...prev.stream!.Message,
+              {
+                id: Date.now(),
+                message: form.message,
+                user: { id: user?.id, avatar: user?.avatar },
+              },
+            ],
+          },
+        } as any),
+      false
+    );
     sendMessage(form);
   };
-  useEffect(() => {
-    if (sendMessageData?.ok) {
-      mutate();
-    }
-  }, [mutate, sendMessageData]);
 
   return (
     <AppBar title={data?.stream?.name} canGoBack>
       <div className="p-4">
         <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-        <div className="mt-5">
-          <h1 className="text-3xl font-bold text-gray-900">
-            {data?.stream?.name}
-          </h1>
-          <span className="text-2xl block mt-3 text-gray-900">
-            ${data?.stream?.price}
-          </span>
-          <p className=" my-6 text-gray-700">{data?.stream?.description}</p>
-        </div>
-        <div>
-          <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
-          <div className="py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4">
-            {data?.stream?.Message.map((message) => {
-              return (
-                <Message
-                  key={message.id}
-                  message={message.message}
-                  reversed={message.user.id === user?.id}
-                />
-              );
-            })}
+        <div className="h-[50vh] overflow-y-scroll">
+          <div className="mt-5">
+            <h1 className="text-3xl font-bold text-gray-900">
+              {data?.stream?.name}
+            </h1>
+            <span className="text-2xl block mt-3 text-gray-900">
+              ${data?.stream?.price}
+            </span>
+            <p className=" my-6 text-gray-700">{data?.stream?.description}</p>
           </div>
-          <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
-            <form
-              onSubmit={handleSubmit(onValid)}
-              className="flex relative max-w-md items-center  w-full mx-auto"
-            >
-              <input
-                type="text"
-                {...register("message", { required: true })}
-                className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
-              />
-              <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-                <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
-                  &rarr;
-                </button>
-              </div>
-            </form>
+          <div>
+            <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
+            <div className="py-10 pb-16 px-4 space-y-4">
+              {data?.stream?.Message.map((message) => {
+                return (
+                  <Message
+                    key={message.id}
+                    message={message.message}
+                    reversed={message.user.id === user?.id}
+                  />
+                );
+              })}
+            </div>
+            <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
+              <form
+                onSubmit={handleSubmit(onValid)}
+                className="flex relative max-w-md items-center  w-full mx-auto"
+              >
+                <input
+                  type="text"
+                  {...register("message", { required: true })}
+                  className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+                />
+                <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+                  <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
+                    &rarr;
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>

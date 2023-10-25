cc80d721532c06a43b8c9c98fd3f598642fa5c31
LeeKyuSung <ks5050577@gmail.com>
2023-10-25 20:55:41 +0900

    #17.2 Streaming API

--- src/app/api/streams/route.ts
+++ src/app/api/streams/route.ts
@@ -24,11 +24,30 @@ export function GET(request: NextRequest) {
 export async function POST(request: Request) {
   return withAuth(async (session) => {
     const { name, price, description } = await request.json();
+
+    const response = await (
+      await fetch(
+        `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/stream/live_inputs`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: `{"meta": {"name":"${name}"},"recording": { "mode": "automatic", "timeoutSeconds": 10}}`,
+        }
+      )
+    ).json();
+    console.log(response);
+
     const stream = await client.stream.create({
       data: {
         name,
         price,
         description,
+        cloudflareId: "uid",
+        cloudflareKey: "streamKey",
+        cloudflareUrl: "url",
         user: {
           connect: {
             id: session.userId,
